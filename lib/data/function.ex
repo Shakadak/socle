bb3f7e84688175383126bcfd79b7967d29fe7e69
nathanael <nathanael.pineau@kbrwadventure.com>
2020-11-27 23:22:17 +0100

    Data.Function

---
+++ lib/data/function.ex
@@ -0,0 +1,22 @@
+defmodule Bask.Data.Function do
+  use Socle.Meta.Curry
+
+  defcurried id(x), do: x
+
+  defcurried const(x, _), do: x
+
+  defcurried compose(f, g, x), do: f.(g.(x))
+  defcurried f .. g, do: fn x -> f.(g.(x)) end
+
+  defcurried flip(f, x, y) when is_function(f, 2), do: f.(y, x)
+  defcurried flip(f, x, y) when is_function(f, 1), do: f.(y).(x)
+
+  defcurried fix(f, x) when is_function(f, 2), do: f.(fix(f), x) # would it even work ?
+  defcurried fix(f, x) when is_function(f, 1), do: f.(fix(f)).(x)
+
+  defcurried on(bi, f, x, y) when is_function(bi, 2), do: bi.(f.(x), f.(y))
+  defcurried on(bi, f, x, y) when is_function(bi, 1), do: bi.(f.(x)).(f.(y))
+
+  defcurried ap(f, x), do: f.(x)
+  defcurried pa(x, f), do: f.(x)
+end
